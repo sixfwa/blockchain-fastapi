5fb9f6320c06e795a11d4c00df6a75e8afc28de8
Farhan Ali <farhanali@farhans-mbp.home>
2021-08-18 02:26:51 +0100

    blockchain added

---
+++ blockchain.py
@@ -0,0 +1,94 @@
+from typing import Dict
+import datetime as _dt
+import hashlib as _hashlib
+import json as _json
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = list()
+        initial_block = self._create_block(data="genesis block", proof=1, previous_hash="0", index=1)
+        self.chain.append(initial_block)
+
+    def mine_block(self, data: str) -> Dict:
+        previous_block = self.get_previous_block()
+        previous_proof = previous_block["proof"]
+        index = len(self.chain) + 1
+        proof =  self._proof_of_work(previous_proof=previous_proof, index=index, data=data)
+        previous_hash = self._hash(block=previous_block)
+        block = self._create_block(data=data, proof=proof, previous_hash=previous_hash, index=index)
+        self.chain.append(block)
+        return block
+
+    def _create_block(self, data: str, proof: int, previous_hash: str, index: int) -> Dict:
+        block = {
+            "index": index,
+            "timestamp": str(_dt.datetime.now()),
+            "data": data,
+            "proof": proof,
+            "previous_hash": previous_hash,
+        }
+
+        return block
+
+    def get_previous_block(self) -> Dict:
+        return self.chain[-1]
+
+    def _to_digest(
+        self, new_proof: int, previous_proof: int, index: int, data: str
+    ) -> bytes:
+        to_digest = str(new_proof ** 2 - previous_proof ** 2 + index) + data
+
+        return to_digest.encode()
+
+    def _proof_of_work(self, previous_proof: str, index: int, data: str) -> int:
+        new_proof = 1
+        check_proof = False
+
+        while not check_proof:
+            to_digest = self._to_digest(new_proof, previous_proof, index, data)
+            hash_operation = _hashlib.sha256(to_digest).hexdigest()
+
+            if hash_operation[:4] == "0000":
+                check_proof = True
+            else:
+                new_proof += 1
+
+        return new_proof
+
+    def _hash(self, block: Dict) -> str:
+        """
+        Hash a block and return the crytographic hash of the block
+        """
+        encoded_block = _json.dumps(block, sort_keys=True).encode()
+
+        return _hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self) -> bool:
+        previous_block = self.chain[0]
+        block_index = 1
+
+        while block_index < len(self.chain):
+            block = self.chain[block_index]
+            # Check if the previous hash of the current block is the same as the hash of it's previous block
+            if block["previous_hash"] != self._hash(previous_block):
+                return False
+
+            previous_proof = previous_block["proof"]
+            index, data, proof = block["index"], block["data"], block["proof"]
+            hash_operation = _hashlib.sha256(
+                self._to_digest(
+                    new_proof=proof,
+                    previous_proof=previous_proof,
+                    index=index,
+                    data=data,
+                )
+            ).hexdigest()
+
+            if hash_operation[:4] != "0000":
+                return False
+
+            previous_block = block
+            block_index += 1
+
+        return True
