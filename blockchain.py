8b56e965b57e216b7880cadd8e6ce458223c4311
Farhan Ali <sixfwali@gmail.com>
2021-09-28 13:12:02 +0200

    black format

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,3 @@
-from typing import Dict
 import datetime as _dt
 import hashlib as _hashlib
 import json as _json
@@ -7,20 +6,28 @@ import json as _json
 class Blockchain:
     def __init__(self):
         self.chain = list()
-        initial_block = self._create_block(data="genesis block", proof=1, previous_hash="0", index=1)
+        initial_block = self._create_block(
+            data="genesis block", proof=1, previous_hash="0", index=1
+        )
         self.chain.append(initial_block)
 
-    def mine_block(self, data: str) -> Dict:
+    def mine_block(self, data: str) -> dict:
         previous_block = self.get_previous_block()
         previous_proof = previous_block["proof"]
         index = len(self.chain) + 1
-        proof =  self._proof_of_work(previous_proof=previous_proof, index=index, data=data)
+        proof = self._proof_of_work(
+            previous_proof=previous_proof, index=index, data=data
+        )
         previous_hash = self._hash(block=previous_block)
-        block = self._create_block(data=data, proof=proof, previous_hash=previous_hash, index=index)
+        block = self._create_block(
+            data=data, proof=proof, previous_hash=previous_hash, index=index
+        )
         self.chain.append(block)
         return block
 
-    def _create_block(self, data: str, proof: int, previous_hash: str, index: int) -> Dict:
+    def _create_block(
+        self, data: str, proof: int, previous_hash: str, index: int
+    ) -> dict:
         block = {
             "index": index,
             "timestamp": str(_dt.datetime.now()),
@@ -31,14 +38,14 @@ class Blockchain:
 
         return block
 
-    def get_previous_block(self) -> Dict:
+    def get_previous_block(self) -> dict:
         return self.chain[-1]
 
     def _to_digest(
         self, new_proof: int, previous_proof: int, index: int, data: str
     ) -> bytes:
         to_digest = str(new_proof ** 2 - previous_proof ** 2 + index) + data
-
+        # It returns an utf-8 encoded version of the string
         return to_digest.encode()
 
     def _proof_of_work(self, previous_proof: str, index: int, data: str) -> int:
@@ -48,7 +55,6 @@ class Blockchain:
         while not check_proof:
             to_digest = self._to_digest(new_proof, previous_proof, index, data)
             hash_operation = _hashlib.sha256(to_digest).hexdigest()
-
             if hash_operation[:4] == "0000":
                 check_proof = True
             else:
@@ -56,7 +62,7 @@ class Blockchain:
 
         return new_proof
 
-    def _hash(self, block: Dict) -> str:
+    def _hash(self, block: dict) -> str:
         """
         Hash a block and return the crytographic hash of the block
         """
